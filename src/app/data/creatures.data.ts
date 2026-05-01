966a5049b4cee20740d0b03a005a93fc74f6649b
gabbus98 <gabriele.filippini1998@gmail.com>
2026-05-01 21:16:13 +0200

    altri fix

--- src/app/data/creatures.data.ts
+++ src/app/data/creatures.data.ts
@@ -12,6 +12,26 @@ export const CREATURES: Creature[] = [
     resistenze: 'Freddo +50%, Fuoco −30%'
   },
   {
+    nome: 'Serpe Dorata', tipo: 'raro', icona: '', dungeon: 'Piramide Ophidiana | Deserto di Tremec',
+    hp: '120', danno: '15–25', 
+    resistenze: 'Freddo +50%, Fuoco −30%'
+  },
+  {
+    nome: 'Cavaliere Ophidiano', tipo: 'comune', icona: '', dungeon: 'Piramide Ophidiana | Deserto di Tremec',
+    hp: '120', danno: '15–25', 
+    resistenze: 'Freddo +50%, Fuoco −30%'
+  },
+  {
+    nome: 'Arcimago Ophidiano', tipo: 'comune', icona: '', dungeon: 'Piramide Ophidiana | Deserto di Tremec',
+    hp: '120', danno: '15–25', 
+    resistenze: 'Freddo +50%, Fuoco −30%'
+  },
+  {
+    nome: 'Matriarca Ophidiana', tipo: 'comune', icona: '', dungeon: 'Piramide Ophidiana | Deserto di Tremec',
+    hp: '120', danno: '15–25', 
+    resistenze: 'Freddo +50%, Fuoco −30%'
+  },
+  {
     nome: 'Incubo', tipo: 'raro', icona: '', dungeon: 'Albero degli impiccati',
     hp: '120', danno: '15–25', 
     resistenze: 'Fuoco +50%, Freddo +20%, Energia +20%, Veleno +50%, Psionico +30%, Sacro -100%, Malefico +50%, Magia +0%',
@@ -22,5 +42,10 @@ export const CREATURES: Creature[] = [
     hp: '120', danno: '15–25', 
     resistenze: 'Fuoco +50%, Freddo +20%, Energia +20%, Veleno +50%, Psionico +30%, Sacro -100%, Malefico +50%, Magia +0%',
     str: '160', dex: '100', int: '80', salute: '299', stamina: '106', mana: '287'
+  },
+  {
+    nome: 'Serpe Argentata', tipo: 'raro', icona: '', dungeon: '',
+    hp: '120', danno: '15–25', 
+    resistenze: 'Fuoco +50%, Freddo +20%, Energia +20%, Veleno +50%, Psionico +30%, Sacro -100%, Malefico +50%, Magia +0%',
   }
 ];
